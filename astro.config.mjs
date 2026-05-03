d81f7418075e3d7eb9294a4f64e44a8232299bda
See Jay <39662993+ChrisOh431@users.noreply.github.com>
2026-05-03 04:41:49 -0500

    feat: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
