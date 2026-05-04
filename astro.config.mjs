350fd2992cc8dc173e3f6befcb2505c58f2685f2
See Jay <39662993+ChrisOh431@users.noreply.github.com>
2026-05-04 05:21:26 -0500

    style: rip out pp-mori in favor of hyperlegible everything

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,45 +13,6 @@ export default defineConfig({
 
   fonts: [
     {
-      name: "PP Mori",
-      cssVariable: "--font-pp-mori",
-      provider: fontProviders.local(),
-      options: {
-        variants: [
-          {
-            src: ["./src/assets/fonts/PPMori-Regular.woff2"],
-            weight: "400",
-            style: "normal",
-          },
-          {
-            src: ["./src/assets/fonts/PPMori-Italic.woff2"],
-            weight: "400",
-            style: "italic",
-          },
-          {
-            src: ["./src/assets/fonts/PPMori-Semibold.woff2"],
-            weight: "600",
-            style: "normal",
-          },
-          {
-            src: ["./src/assets/fonts/PPMori-SemiboldItalic.woff2"],
-            weight: "600",
-            style: "italic",
-          },
-          {
-            src: ["./src/assets/fonts/PPMori-Black.woff2"],
-            weight: "900",
-            style: "normal",
-          },
-          {
-            src: ["./src/assets/fonts/PPMori-BlackItalic.woff2"],
-            weight: "900",
-            style: "italic",
-          },
-        ],
-      },
-    },
-    {
       name: "Atkinson Hyperlegible Next",
       cssVariable: "--font-hyperlegible",
       provider: fontProviders.local(),
